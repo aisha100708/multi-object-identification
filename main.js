add650e97cb9a866f3e52891afb06de26e579f7b
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2022-02-05 17:33:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 img = "";
+status1 = "";
 function preload() {
     img = loadImage("dog_cat.jpg");
 }
 function setup() {
     canvas = createCanvas(600, 400);
     canvas.center();
-
+    object_detector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 function draw() {
     image(img, 0, 0, 600, 400);
-    fill("black");
     textSize(20);
-    //textBounds(100, 70, 20);
-    //background(1);
+    fill("blue");
     text("dog", 100, 70);
     noFill();
-    rect(80, 70, 220, 310)
-}   
+    stroke("black");
+    rect(80, 70, 220, 310);
+    fill("red");
+    text("cat", 300, 90);
+    noFill();
+    stroke("red");
+    rect(280, 90, 220, 270);
+}
+function modelLoaded() {
+    console.log("model loaded!");
+    status1 = true;
+    object_detector.detect(img, getResults);
+}
+function getResults(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        
+    }
+}
